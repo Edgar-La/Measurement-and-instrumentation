29575a787566134780e43a3cb37a2d69db73036f
Edgar-La <laraae2018@licifug.ugto.mx>
2020-08-23 10:30:45 -0500

    Minus changes

--- 03M1-Estimation_Thevenin_Resistance/0301_simple.py
+++ 03M1-Estimation_Thevenin_Resistance/0301_simple.py
@@ -12,9 +12,11 @@ R4 = float(input('R4 = '))
 Vo = (Vs*R3*(R2+R4))/(R3*(-R1-R2+R4)-R2*(3*R2+2*R1)-R4*(R1+R2))
 Isc = (-1*Vs*(R2+R4))/(R2*(2*R1+3*R2+R4)+R1*R4)
 Rth = Vo/Isc
-Rth_ = (-R3*(R2*(2*R1+3*R2+R4)+R1*R4))/(R3*(-R1-R2+R4)-R2*(3*R2+2*R1)-R4*(R1+R2))
+#Rth_ = (-R3*(R2*(2*R1+3*R2+R4)+R1*R4))/(R3*(-R1-R2+R4)-R2*(3*R2+2*R1)-R4*(R1+R2))
 
 
 print('\n\nVo = ' + str(Vo))
 print('Isc = ' + str(Isc))
-print('Thevenin resistance is Rth = ' + str(Rth))
+print('Rth = ' + str(Rth))
+
+input('\n\nEnter to close...')
