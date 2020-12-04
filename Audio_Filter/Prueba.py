9e6b738ad35e2e661a5cb98ab0e733acfb8c65bb
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-12-03 20:40:24 -0600

    Python code updated

--- Audio_Filter/Prueba.py
+++ Audio_Filter/Prueba.py
@@ -2,9 +2,24 @@ import matplotlib.pyplot as plt #modulo para graficar
 import sounddevice as sd #modulo para grabar audio
 import soundfile as sf #modulo para abrir archivo audio
 from scipy.io.wavfile import write #modulo para guardar audio
+from scipy.fft import fft, ifft, fftshift
+from scipy.signal import butter, filtfilt
 import numpy as np
+from math import pi
+from time import sleep
 import os
-os.system('clear')
+#os.system('clear')
+
+############################################################################################################
+#Seccion de funciones
+def nextpow2(Longy):
+	pot = 0
+	while 2**pot <= Longy:
+		NePw = 2**pot
+		pot+=1
+	return (pot)
+
+############################################################################################################
 fs = 44100
 seconds = 2
 '''
@@ -14,27 +29,79 @@ myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
 sd.wait()
 print('Grabado'); print('Guardando...')
 #Guarda el audio
-write('Grabacion.wav', fs, myrecording)
+write('Grabacion_E.wav', fs, myrecording)
 print('Guardado')
-
+y,fs = sf.read('Grabacion_E.wav')
 '''
+
 #Abre archivo de audio
 y,fs = sf.read('Grabacion.wav')
+
 Longy = len(y)
 Diftime = seconds/Longy;
-Freq = 1/Diftime;
-#time = 0:Diftime:seconds-Diftime;
 time = np.arange(0, seconds, Diftime).tolist()
-#Longtime = len(time);
-print(len(y))
-print(len(time))
 
 #Reproduce audio
-sd.play(y*20)
+#sd.play(y*50); sd.wait() 
+#-------------------------------------------------------------------------
+# FFT de la señal de audio (RADIOGRAFÍA DE LA SEÑAL)
+AudioAnalysis = 2**nextpow2(Longy)		#NUMERO DE MUESTRAS
+AudiOriginal = fft(y, AudioAnalysis)	#Aplicamos la FFT
+f1 = fs*(np.arange((-AudioAnalysis/2), (AudioAnalysis/2)))/AudioAnalysis
+AudiOrg=fftshift(AudiOriginal)			#ordenar de mayor a menor las fft
+
+#Calculo de la magnitud
+b1=2*abs(AudiOrg);
+logY1=b1**2;
+
+
+#Calculo de la fase
+PhaseAudio=np.angle(AudiOrg);
+Angle=(180/pi)*PhaseAudio
+
+#-------------------------------------------------------------------------
+# Frecuencias de corte para análisis de la señal
+flow = 400								#frecuencia baja
+fhigh = 800								#frecuencia alta
+wclow=flow*1.25*pi
+wchigh=fhigh*1.25*pi
+ffiltro=Longy/max(time)
+
+#Filtro de la señal de pasa-banda
+Long_OriginalSignal = len(Angle)
+ffiltro1 = ffiltro*Long_OriginalSignal
+Wnd1=[wclow/ffiltro, wchigh/ffiltro]
+f1a,g1a = butter(4, [Wnd1[0]/2, Wnd1[1]/2,], btype='band')
+FilteredSignal= filtfilt(f1a,g1a,y)
+sd.play(FilteredSignal*50); sd.wait()
+
+#-------------------------------------------------------------------------
+# FFT de la señal de audio (RADIOGRAFÍA DE LA SEÑAL)
+AudioAnalysis2 = 2**nextpow2(Longy)		#NUMERO DE MUESTRAS
+AudiOriginal2 = fft(FilteredSignal, AudioAnalysis2)	#Aplicamos la FFT
+f2 = fs*(np.arange((-AudioAnalysis2/2), (AudioAnalysis2/2)))/AudioAnalysis2
+AudiOrg2=fftshift(AudiOriginal2)			#ordenar de mayor a menor las fft
+
+#Calculo de la magnitud
+b2=2*abs(AudiOrg2)
+logY2=b2**2
+
+
+#Calculo de la fase
+PhaseAudio2=np.angle(AudiOrg2)
+Angle2=(180/pi)*PhaseAudio2
+
+#-------------------------------------------------------------------------
 
 #Grafica la señal de audio
-#plt.plot(y, label = 'y')
-plt.plot(time, y, label = 'time vs y', linewidth=0.5)
+F_3 = plt.plot(f1,logY1,'r', f2,logY2,'b', label = 'time vs y', linewidth=0.5)
+plt.legend()
+plt.grid(color='k', linestyle='--', linewidth=0.5)
+plt.show()
+
+
+F_4 = plt.plot(y,'b', label = 'time vs y', linewidth=0.5)
+plt.plot(FilteredSignal,'r', label = 'time vs y', linewidth=0.5)
 plt.legend()
 plt.grid(color='k', linestyle='--', linewidth=0.5)
 plt.show()
