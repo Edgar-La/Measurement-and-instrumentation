896d772d8bbe209c06fe471ee330a2f3ba4f938f
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-12-03 22:57:22 -0600

    Code updated

--- Audio_Filter/Prueba.py
+++ Audio_Filter/Prueba.py
@@ -21,7 +21,7 @@ def nextpow2(Longy):
 
 ############################################################################################################
 fs = 44100
-seconds = 2
+#seconds = 9
 '''
 #Grabar audio
 print('Grabando...')
@@ -35,18 +35,23 @@ y,fs = sf.read('Grabacion_E.wav')
 '''
 
 #Abre archivo de audio
-y,fs = sf.read('Grabacion.wav')
-
+y,fs = sf.read('CRACK1.wav')
 Longy = len(y)
-Diftime = seconds/Longy;
+#Diftime = seconds/Longy;
+seconds = Longy/fs
+Diftime = 1/fs
 time = np.arange(0, seconds, Diftime).tolist()
-
 #Reproduce audio
-#sd.play(y*50); sd.wait() 
+'''
+sd.play(y*50);
+sd.wait()
+sd.wait()
+sd.wait()'''
 #-------------------------------------------------------------------------
 # FFT de la señal de audio (RADIOGRAFÍA DE LA SEÑAL)
 AudioAnalysis = 2**nextpow2(Longy)		#NUMERO DE MUESTRAS
 AudiOriginal = fft(y, AudioAnalysis)	#Aplicamos la FFT
+
 f1 = fs*(np.arange((-AudioAnalysis/2), (AudioAnalysis/2)))/AudioAnalysis
 AudiOrg=fftshift(AudiOriginal)			#ordenar de mayor a menor las fft
 
@@ -66,6 +71,7 @@ fhigh = 800								#frecuencia alta
 wclow=flow*1.25*pi
 wchigh=fhigh*1.25*pi
 ffiltro=Longy/max(time)
+#ffiltro=Longy/time
 
 #Filtro de la señal de pasa-banda
 Long_OriginalSignal = len(Angle)
@@ -73,7 +79,7 @@ ffiltro1 = ffiltro*Long_OriginalSignal
 Wnd1=[wclow/ffiltro, wchigh/ffiltro]
 f1a,g1a = butter(4, [Wnd1[0]/2, Wnd1[1]/2,], btype='band')
 FilteredSignal= filtfilt(f1a,g1a,y)
-sd.play(FilteredSignal*50); sd.wait()
+#sd.play(FilteredSignal*50); sd.wait()
 
 #-------------------------------------------------------------------------
 # FFT de la señal de audio (RADIOGRAFÍA DE LA SEÑAL)
@@ -94,7 +100,7 @@ Angle2=(180/pi)*PhaseAudio2
 #-------------------------------------------------------------------------
 
 #Grafica la señal de audio
-F_3 = plt.plot(f1,logY1,'r', f2,logY2,'b', label = 'time vs y', linewidth=0.5)
+F_3 = plt.plot(f1,logY1,'b', f2,logY2,'r', label = 'time vs y', linewidth=0.5)
 plt.legend()
 plt.grid(color='k', linestyle='--', linewidth=0.5)
 plt.show()
