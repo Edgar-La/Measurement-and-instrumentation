d1a287f886363f6e91f8c33245f4e4a9dde10da0
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-12-05 22:24:37 -0600

    Dropdown menu added

--- Audio_Filter/Prueba_GUI.py
+++ Audio_Filter/Prueba_GUI.py
@@ -7,17 +7,8 @@ def delete_values():
 	F_low.set("")
 	F_high.set("")
 	Name_File.set("")
-	R3.set("")
-	R4.set("")
 
-def calculate(Vs_, R1_, R2_, R3_, R4_):
-	Vo_ = (Vs_*R3_*(R2_+R4_))/(R3_*(-R1_-R2_+R4_)-R2_*(3*R2_+2*R1_)-R4_*(R1_+R2_))
-	Isc_ = (-1*Vs_*(R2_+R4_))/(R2_*(2*R1_+3*R2_+R4_)+R1_*R4_)
-	Rth_ = Vo_/Isc_
-	Vo.set(Vo_); Isc.set(Isc_); Rth.set(Rth_);
-	Vo_label_ = Label(root, textvariable = str(Vo)).grid(row = 6, column = 1)
-	Isc_label_ = Label(root, textvariable = str(Isc)).grid(row = 7, column = 1)
-	Rth_label_ = Label(root, textvariable = str(Rth)).grid(row = 8, column = 1)
+
 
 def close_window(): 
 	    root.destroy()
@@ -39,34 +30,23 @@ image_label = Label(image = image).grid(row = 0, padx=27, pady=20, columnspan=5)
 F_low_label = Label(root, text = "F baja = ").grid(row = 3, column = 0)
 F_high_label = Label(root, text = "F alta = ").grid(row = 4, column = 0)
 Name_File_label = Label(root, text = "Nombre archivo:").grid(row = 3, column = 2,  padx=2, pady=2)
+#Section_Record = Label(root, bg = '#20B2AA', text = "    Controles de audio    ").grid(row = 1, column = 3,  padx=2, pady=2)
 Section_Record = Label(root, bg = '#20B2AA', text = "    Controles de audio    ").grid(row = 1, column = 3,  padx=2, pady=2)
-#Section_ = Label(root, bg = '#20B2AA', text = "Controles pregrabado").grid(row = 1, column = 4)
-#R3_label = Label(root, text = "R3 = ").grid(row = 4, column = 0)
-#R4_label = Label(root, text = "R4 = ").grid(row = 5, column = 0)
-#Vo_label = Label(root, text = "Vo = ").grid(row = 6, column = 0)
-#Isc_label = Label(root, text = "Isc = ").grid(row = 7, column = 0)
-#Rth_label = Label(root, text = "Rth = ").grid(row = 8, column = 0)
 
+
+Name_File_box = StringVar(); Name_File_box.set('Grabacion.wav')
 F_low__entry = Entry(root, textvariable = F_low, width =10).grid(row = 3, column = 1)
 F_high_entry = Entry(root, textvariable = F_high, width =10).grid(row = 4, column = 1)
 Name_File_entry = Entry(root, textvariable = Name_File).grid(row = 3, column = 3)
-#R3_entry = Entry(root, textvariable = R3).grid(row = 4, column = 1)
-#R4_entry = Entry(root, textvariable = R4).grid(row = 5, column = 1)
-#Vo_label_ = Label(root, textvariable = str(Vo)).grid(row = 6, column = 1)
-#Isc_label_ = Label(root, textvariable = str(Isc)).grid(row = 7, column = 1)
-#Rth_label_ = Label(root, textvariable = str(Rth)).grid(row = 8, column = 1)
+files_names = ['Grabacion.wav', 'Test.wav','Grabacion_E.wav']
+Name_file_box = OptionMenu(root, Name_File_box, *files_names).grid(row = 9, column = 3)
+
 
 Erase_button = Button(root, bg ='#008B8B', fg='white', text = "Limpiar valores", width =15, command = delete_values).grid(row = 8, column = 0, padx=2, pady=2)
-#Calculate_button = Button(root, bg ='blue', fg='white', text = "Calculate", command = lambda: calculate(float(Vs.get()), float(R1.get()), float(R2.get()), float(R3.get()), float(R4.get())))
-#Calculate_button.grid(row = 9, column = 1)
 Record_button = Button(root, bg ='#008B8B', fg='white', text = "Grabar", width =20).grid(row = 2, column = 3, padx=2, pady=2)
 Play_Rec_O_button = Button(root, bg ='#483D8B', fg='white', text = "Play Original", width =20).grid(row = 4, column = 3, padx=2, pady=2)
 Play_Rec_F_button = Button(root, bg ='#483D8B', fg='white', text = "Play Filtrado", width =20).grid(row = 5, column = 3, padx=2, pady=2)
 Plot_Rec_button = Button(root, bg ='#483D8B', fg='white', text = "Graficar", width =20).grid(row = 6, column = 3, padx=2, pady=2)
 
-#Play_File_O_button = Button(root, bg ='#483D8B', fg='white', text = "Play Original", width =20).grid(row = 3, column = 4, padx=2, pady=2)
-#Play_File_F_button = Button(root, bg ='#483D8B', fg='white', text = "Play Filtrado", width =20).grid(row = 4, column = 4, padx=2, pady=2)
-#Plot_File_button = Button(root, bg ='#483D8B', fg='white', text = "Graficar", width =20).grid(row = 5, column = 4, padx=2, pady=2)
-#Plot_button = Button(root, bg ='blue', fg='white', text = "Graficar").grid(row = 9, column = 3)
 my_button_close = Button(root, text="Cerrar ventana", bg ='red', fg='white', width =20, command=close_window).grid(row = 8, column = 3) 
 root.mainloop()
