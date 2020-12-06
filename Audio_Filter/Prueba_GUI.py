812952d45358721acbea280be467576420b85d5b
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-12-05 22:43:51 -0600

    Combobox added

--- Audio_Filter/Prueba_GUI.py
+++ Audio_Filter/Prueba_GUI.py
@@ -2,13 +2,12 @@ import os
 os.system('clear')
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import ttk
 
 def delete_values():
 	F_low.set("")
 	F_high.set("")
 	Name_File.set("")
-
-
 
 def close_window(): 
 	    root.destroy()
@@ -20,27 +19,26 @@ root.title('Filtro Pasa-Banda -- Edgar Lara')
 root.configure(background='#696969')
 image = PhotoImage( file = "IMG_GUI.png" )
 
-F_low = StringVar(); F_high = StringVar(); Name_File = StringVar(); R3 = StringVar(); R4 = StringVar();
-F_low.set(400); F_high.set(800); Name_File.set('Grabacion.wav'); R3.set(500); R4.set(10000);
-Vo = StringVar(); Isc = StringVar(); Rth = StringVar();
-Vo.set(""); Isc.set(""); Rth.set("") 
+F_low = StringVar(); F_high = StringVar(); #Name_File = StringVar();
+F_low.set(400); F_high.set(800); #Name_File.set('Grabacion.wav');
 
 image_label = Label(image = image).grid(row = 0, padx=27, pady=20, columnspan=5)
 
 F_low_label = Label(root, text = "F baja = ").grid(row = 3, column = 0)
 F_high_label = Label(root, text = "F alta = ").grid(row = 4, column = 0)
 Name_File_label = Label(root, text = "Nombre archivo:").grid(row = 3, column = 2,  padx=2, pady=2)
-#Section_Record = Label(root, bg = '#20B2AA', text = "    Controles de audio    ").grid(row = 1, column = 3,  padx=2, pady=2)
+Section_Record = Label(root, bg = '#20B2AA', text = "    Controles de audio    ").grid(row = 1, column = 3,  padx=2, pady=2)
 Section_Record = Label(root, bg = '#20B2AA', text = "    Controles de audio    ").grid(row = 1, column = 3,  padx=2, pady=2)
 
-
-Name_File_box = StringVar(); Name_File_box.set('Grabacion.wav')
+files_names = ['Grabacion.wav', 'Test.wav','Grabacion_E.wav']
+Name_File_box = StringVar(); Name_File_box.set(files_names[0])
 F_low__entry = Entry(root, textvariable = F_low, width =10).grid(row = 3, column = 1)
 F_high_entry = Entry(root, textvariable = F_high, width =10).grid(row = 4, column = 1)
-Name_File_entry = Entry(root, textvariable = Name_File).grid(row = 3, column = 3)
-files_names = ['Grabacion.wav', 'Test.wav','Grabacion_E.wav']
-Name_file_box = OptionMenu(root, Name_File_box, *files_names).grid(row = 9, column = 3)
-
+#Name_File_entry = Entry(root, textvariable = Name_File).grid(row = 3, column = 3)
+#Name_file_box = OptionMenu(root, Name_File_box, *files_names).grid(row = 9, column = 3)
+Name_file_box = ttk.Combobox(root, value = files_names, width =18)
+Name_file_box.current(0)
+Name_file_box.grid(row = 3, column = 3)
 
 Erase_button = Button(root, bg ='#008B8B', fg='white', text = "Limpiar valores", width =15, command = delete_values).grid(row = 8, column = 0, padx=2, pady=2)
 Record_button = Button(root, bg ='#008B8B', fg='white', text = "Grabar", width =20).grid(row = 2, column = 3, padx=2, pady=2)
